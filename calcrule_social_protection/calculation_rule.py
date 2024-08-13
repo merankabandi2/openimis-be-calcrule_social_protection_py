b5e01f7a94348b6fbc09b6ce5c4c5bbe05fa95c2
delcroip <patrick.delcroix@swisstph.ch>
2024-08-13 16:22:32 +0200

    feature/remove-signals

--- calcrule_social_protection/calculation_rule.py
+++ calcrule_social_protection/calculation_rule.py
@@ -2,12 +2,14 @@ from calcrule_social_protection.strategies import (
     BenefitPackageStrategyStorage
 )
 from calcrule_social_protection.config import CLASS_RULE_PARAM_VALIDATION, DESCRIPTION_CONTRIBUTION_VALUATION, FROM_TO
-from core.abs_calculation_rule import AbsCalculationRule
+from core.abs_calculation_rule import AbsStrategy
 from core.signals import *
 from core import datetime
 from contribution_plan.models import PaymentPlan
+from django.contrib.contenttypes.models import ContentType
 
-class SocialProtectionCalculationRule(AbsCalculationRule):
+
+class SocialProtectionCalculationRule(AbsStrategy):
     version = 1
     uuid = "32d96b58-898a-460a-b357-5fd4b95cd87c"
     calculation_rule_name = "Calculation rule: social protection"
@@ -21,27 +23,7 @@ class SocialProtectionCalculationRule(AbsCalculationRule):
     sub_type = "benefit_plan"
     CLASS_NAME_CHECK = ['PaymentPlan']
 
-    signal_get_rule_name = Signal([])
-    signal_get_rule_details = Signal([])
-    signal_get_param = Signal([])
-    signal_get_linked_class = Signal([])
-    signal_calculate_event = Signal([])
-    signal_convert_from_to = Signal([])
 
-    @classmethod
-    def ready(cls):
-        now = datetime.datetime.now()
-        condition_is_valid = (now >= cls.date_valid_from and now <= cls.date_valid_to) \
-            if cls.date_valid_to else (now >= cls.date_valid_from and cls.date_valid_to is None)
-        if condition_is_valid:
-            if cls.status == "active":
-                # register signals getParameter to getParameter signal and getLinkedClass ot getLinkedClass signal
-                cls.signal_get_rule_name.connect(cls.get_rule_name, dispatch_uid="on_get_rule_name_signal")
-                cls.signal_get_rule_details.connect(cls.get_rule_details, dispatch_uid="on_get_rule_details_signal")
-                cls.signal_get_param.connect(cls.get_parameters, dispatch_uid="on_get_param_signal")
-                cls.signal_get_linked_class.connect(cls.get_linked_class, dispatch_uid="on_get_linked_class_signal")
-                cls.signal_calculate_event.connect(cls.run_calculation_rules, dispatch_uid="on_calculate_event_signal")
-                cls.signal_convert_from_to.connect(cls.run_convert, dispatch_uid="on_convert_from_to")
 
     @classmethod
     def run_calculation_rules(cls, sender, instance, user, context, **kwargs):
@@ -61,7 +43,14 @@ class SocialProtectionCalculationRule(AbsCalculationRule):
 
     @classmethod
     def get_linked_class(cls, sender, class_name, **kwargs):
-        return ["Calculation"]
+        list_class = super().get_linked_class(sender, class_name, **kwargs)
+        if class_name == "PaymentPlan":
+            list_class.append("Calculation")
+        if class_name == "BenefitPlan":
+            list_class.append("PaymentPlan")
+        if class_name == "Beneficiary":
+            list_class.append("BenefitPlan")
+        return list_class
 
     @classmethod
     def get_parameters(cls, sender, class_name, instance, **kwargs):
